c024a26d64d6b68fdde384276a4feb7eebf63359
Gordon Stewart <gstew5@gmail.com>
2020-02-13 14:44:25 -0500

    How to use ByteOrder

--- in-class/binary/src/main.rs
+++ in-class/binary/src/main.rs
@@ -1,17 +1,24 @@
 use std::fs::OpenOptions;
 use std::io::prelude::*;
 use std::io::SeekFrom;
+use byteorder::{ByteOrder,BigEndian,LittleEndian};
 
 //Create, write, then read a binary file.
 //To display the resulting file test.o, do:
 // od --endian big -A x -x test.o
 fn main() -> std::io::Result<()> {
-    let bytes: Vec<u8> = vec![104, 101, 108, 108, 111, 33];
+    let mut bytes: Vec<u8> = vec![104, 101, 108, 108, 111, 33];
 
     //Create file
     let mut file = OpenOptions::new()
         .read(true).write(true).open("test.o")?;
 
+    //Write bytes 0x48, then 0x45 to the beginning of the vector
+    BigEndian::write_u16(&mut bytes, 0x48_45);
+
+    //Write bytes 0x45, then 0x48 to the beginning of the vector
+    //LittleEndian::write_u16(&mut bytes, 0x48_45);
+    
     //Write bytes
     file.write_all(&bytes[..])?;
 
